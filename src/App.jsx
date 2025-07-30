d9d9f9c76caf04ef84b3821729e52572935da6e3
desoff2026 <luismiguel200365@gmail.com>
2025-07-30 14:54:59 -0500

    modifique contenedor de app para que no les aparezca y login

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import { Routes, Route } from 'react-router-dom'; // 👈 ¡IMPORTANTE!
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
+import DisasterView from './pages/DisasterView';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import './App.css';
+
 
 function App() {
   return (
     <>
       <Navbar />
-       <div className="main-content">
-      <div className="form-container">
-        <div className="form-box login-container">
+       
+     
+       
+          
         <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
+        <Route path="/" element={<DisasterView />} />
       </Routes>
-      </div>
-    </div>
-      </div>
+      
+   
+     
 
       <Footer />
     </>
